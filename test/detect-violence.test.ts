10032f8c1bcdfa0420d57ccbc0043e45d4bfa654
Ashel Vasquez <vasquezpalominoashel@gmail.com>
2024-06-21 22:51:37 -0500

    Initial commit

---
+++ test/detect-violence.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DetectViolence from '../lib/detect-violence-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/detect-violence-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DetectViolence.DetectViolenceStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
